1752f37fbde0ec51894f5cae92520fb437ed62ea
Leandro <devleandromcz@gmail.com>
2024-10-07 19:20:15 -0300

    Refactor Entregador class to extend Usuario and add getters and setters for veiculo and placa

--- src/br/ufal/ic/p2/jackut/Entregador.java
+++ src/br/ufal/ic/p2/jackut/Entregador.java
@@ -1,23 +1,30 @@
 package br.ufal.ic.p2.jackut;
 
 // nome=<String> email=<String> senha=<String> endereco=<String> veiculo=<String> endereco=<placa>
-public class Entregador {
-    private static int contador = 0;
-    public int id;
-    public String nome;
-    public String email;
-    public String senha;
-    public String endereco;
-    public String veiculo;
-    public String placa;
+public class Entregador extends Usuario {
+    private String veiculo;
+    private String placa;
 
     public Entregador(String nome, String email, String senha, String endereco, String veiculo, String placa) {
-        this.id = contador++;
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-        this.endereco = endereco;
+        super(nome, email, senha, endereco);
         this.veiculo = veiculo;
         this.placa = placa;
     }
+
+    public String getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(String veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
 }
