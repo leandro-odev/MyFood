31aabd0baf8ee5ffbf5182a8ead330eb289e20dc
Caio <caioagralemos@live.com>
2024-10-11 19:35:26 -0300

    entregador e entrega

---
+++ src/br/ufal/ic/p2/jackut/Entrega.java
@@ -0,0 +1,24 @@
+package br.ufal.ic.p2.jackut;
+
+import java.util.ArrayList;
+
+public class Entrega {
+    private static int contador = 0;
+    public int id;
+    public String cliente;
+    public String empresa;
+    public Integer pedido;
+    public Integer entregador;
+    public String destino;
+    public ArrayList<String> produtos;
+
+    public Entrega(String cliente, String empresa, Integer pedido, Integer entregador, String destino, ArrayList<String> produtos) {
+        this.id = contador++;
+        this.cliente = cliente;
+        this.empresa = empresa;
+        this.pedido = pedido;
+        this.entregador = entregador;
+        this.destino = destino;
+        this.produtos = produtos;
+    }
+}
--- src/br/ufal/ic/p2/jackut/Entregador.java
+++ src/br/ufal/ic/p2/jackut/Entregador.java
@@ -1,9 +1,13 @@
 package br.ufal.ic.p2.jackut;
 
+import java.util.ArrayList;
+
 // nome=<String> email=<String> senha=<String> endereco=<String> veiculo=<String> endereco=<placa>
 public class Entregador extends User {
     public String veiculo;
     public String placa;
+    public ArrayList<Integer> empresas;
+    public Boolean ocupado;
 
     public Entregador(String nome, String email, String senha, String endereco, String veiculo, String placa) {
         super(nome, email, senha, endereco);
